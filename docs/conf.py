7be6df4423a4ae9e780febe8de722dcdd3b8ba05
Cannon Lock <clock@wisc.edu>
2021-11-04 13:13:56 -0500

    Update RTD config

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 
 import glob
 
-print([glob for glob in glob.glob("./**")])
+print([glob for glob in glob.glob("./**", recursive=True)])
 
 # -- Path setup --------------------------------------------------------------
 
@@ -53,7 +53,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['./_static']
+
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
